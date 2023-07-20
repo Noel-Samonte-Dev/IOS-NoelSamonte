9eded8e9fab6123888980c04672313976c0ad1d8
Noel Samontes <n.samonte@synermaxx.com>
2023-07-20 12:17:28 +0800

    Initial Commit

---
+++ App Dev Test Project/App_Dev_Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Dev_Test_ProjectApp.swift
+//  App Dev Test Project
+//
+//  Created by noelsamonte on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct App_Dev_Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
